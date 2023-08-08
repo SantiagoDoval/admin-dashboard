efd1a513adbdbe7f1f2c192b928fcceebdca1fe8
SantiagoDoval <danieldovals@gmail.com>
2023-08-08 17:25:33 -0500

    Feature: Theme Fixes

--- src/App.js
+++ src/App.js
@@ -29,18 +29,19 @@ import { useStateContext } from "./context/ContextProvider";
 import "./App.css";
 
 const App = () => {
-  const { activeMenu } = useStateContext();
+  const { activeMenu, themeSettings, setThemeSettings , currentColor, currentMode } = useStateContext();
 
   return (
-    <div>
+    <div className={currentMode === 'Dark' ?'dark':''}>
       <BrowserRouter>
         <div className="flex relative dark:bg-main-dark-bg">
           <div className="fixed right-4 bottom-4" style={{ zIndex: "1000" }}>
             <TooltipComponent content="Settings" position="LeftCenter">
               <button
                 type="button"
+                onClick={()=>{setThemeSettings(true)}}
                 className="text-3xl p-3 hover:drop-shadow-xl hover:bg-light-gray text-white"
-                style={{ background: "blue", borderRadius: "50%" }}>
+                style={{ background: currentColor, borderRadius: "50%" }}>
                 <FiSettings />
               </button>
             </TooltipComponent>
@@ -55,7 +56,7 @@ const App = () => {
             </div>
           )}
           <div
-            className={`dark:bg-main-bg bg-main-bg min-h-screen w-full ${
+            className={`dark:bg-main-dark-bg bg-main-bg min-h-screen w-full ${
               activeMenu ? "md:ml-72" : "flex-2"
             }`}>
             <div className="fixed md:static bg-main-bg dark:bg-main-dark-bg navbar w-full">
@@ -63,6 +64,8 @@ const App = () => {
             </div>
 
             <div>
+
+              {themeSettings && <ThemeSettings />}
               <Routes>
                 <Route path="/" element={<Ecommerce />} />
                 <Route path="/ecommerce" element={<Ecommerce />} />
--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -11,7 +11,7 @@ import { useStateContext } from '../context/ContextProvider'
 
 const Sidebar = () => {
 
-  const {activeMenu,setActiveMenu, screenSize}=useStateContext();
+  const {activeMenu,setActiveMenu, screenSize, currentColor}=useStateContext();
 
   const handleCloseSideBar = () => {
     if(activeMenu && screenSize <=900){
@@ -19,7 +19,7 @@ const Sidebar = () => {
     }
   }
 
-  const activeLink='flex items-center gap-5 pl-4 pt-3 pb-2.5 rounded-lg text-gray-900 text-md m-2';
+  const activeLink='flex items-center gap-5 pl-4 pt-3 pb-2.5 rounded-lg text-white text-md m-2';
   const normalLink='flex items-center gap-5 pl-4 pt-3 pb-2.5 rounded-lg text-md text-gray-400 dark:text-gray-200 dark:hover:text-black hover:bg-light-gray m-2';
 
   
@@ -52,6 +52,7 @@ const Sidebar = () => {
                     to={`/${link.name}`}
                     key={link.name}
                     onClick={handleCloseSideBar}
+                    style={({ isActive})=>({backgroundColor: isActive? currentColor:''})}
                     className={({isActive})=>( isActive ? activeLink : normalLink)}>
                       {link.icon}
                       <span className='capitalize'>
